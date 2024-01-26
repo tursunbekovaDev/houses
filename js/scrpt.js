18d2e23671f9893c5c2235d6fbfd0d4b17619298
Your Name <you@example.com>
2024-01-27 00:45:31 +0300

    first

---
+++ js/scrpt.js
@@ -0,0 +1,57 @@
+let swiper = new Swiper(".mySwiper", {
+	spaceBetween: 30,
+	centeredSlides: true,
+	autoplay: {
+	  delay: 2000,
+	  disableOnInteraction: false,
+	},
+	pagination: {
+	  el: ".swiper-pagination",
+	  clickable: true,
+	},
+	loop:true,
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+ });
+ let swiper2 = new Swiper(".mySwiper2", {
+	spaceBetween: 30,
+	centeredSlides: true,
+	slidesPerView: 3,
+	autoplay: {
+	  delay: 2000,
+	  disableOnInteraction: false,
+	},
+	pagination: {
+	  el: ".swiper-pagination",
+	  clickable: true,
+	},
+	loop:true,
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+ });
+
+
+ let swiper3 = new Swiper(".mySwiper3", {
+	spaceBetween: 30,
+	centeredSlides: true,
+	slidesPerView: 2,
+	autoplay: {
+	  delay: 2000,
+	  disableOnInteraction: false,
+	},
+	pagination: {
+	  el: ".swiper-pagination",
+	  clickable: true,
+	},
+	loop:true,
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+ });
+
+ 
